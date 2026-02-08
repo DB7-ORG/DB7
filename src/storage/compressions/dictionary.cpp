c50855551447a451026123915e355076df496862
JovanKatanic <jovan.katanic0101@gmail.com>
2026-02-08 22:15:27 +0100

    dict refaq

--- src/storage/compressions/dictionary.cpp
+++ src/storage/compressions/dictionary.cpp
@@ -97,8 +97,31 @@ inline int get_bits_used(u32 value)
     return 32 - __builtin_clz(value);
 }
 
+u32 *serialize(u32 *out, u32 *indexes, u8 *strings, u32 *data, u32 idx, u32 count)
+{
+    // serialization
+    u32 usedBits = get_bits_used(idx);
+    out[0] = usedBits;
+    out++;
+    u32 *initidx = (u32 *)BitPackEncoder::scalar_encode(out, data, count, usedBits);
+
+    initidx[0] = idx;
+    initidx++;
+
+    u32 usedBitsIdx = get_bits_used(indexes[idx - 1]);
+    initidx[0] = usedBitsIdx;
+    initidx++;
+    u32 *initstr = (u32 *)BitPackEncoder::scalar_encode(initidx, indexes, idx, usedBitsIdx);
+
+    u32 strsize = indexes[idx - 1];
+    memcpy(initstr, strings, strsize);
+
+    return initstr + strsize;
+}
+
 u32 *DictionaryEncoder::encode(u32 *out, u8 **in, u32 *lenIn, u32 count, u32 strLen)
 {
+    // TODO should be passed as parameter to function
     u8 *strings = (u8 *)malloc(strLen);                      // TODO this is not len i want, should use vec w allocators
     u32 *indexes = (u32 *)malloc((count + 1) * sizeof(u32)); // TODO this is not len i want, should use vec w allocators
     u32 *data = (u32 *)malloc(count * sizeof(u32));
@@ -122,24 +145,7 @@ u32 *DictionaryEncoder::encode(u32 *out, u8 **in, u32 *lenIn, u32 count, u32 str
         }
     }
 
-    // serialization
-    u32 usedBits = get_bits_used(idx);
-    out[0] = usedBits;
-    out++;
-    u32 *initidx = (u32 *)BitPackEncoder::scalar_encode(out, data, count, usedBits);
-
-    initidx[0] = idx;
-    initidx++;
-
-    u32 usedBitsIdx = get_bits_used(indexes[idx - 1]); // TODO add usedBits
-    initidx[0] = usedBitsIdx;
-    initidx++;
-    u32 *initstr = (u32 *)BitPackEncoder::scalar_encode(initidx, indexes, idx, usedBitsIdx);
-
-    u32 strsize = indexes[idx - 1];
-    memcpy(initstr, strings, strsize);
-
-    return initstr + strsize;
+    return serialize(out, indexes, strings, data, idx, count);
 }
 
 u32 *DictionaryEncoder::decode(u8 **out, u32 *lenOut, const u32 *in, u32 count)
@@ -148,7 +154,9 @@ u32 *DictionaryEncoder::decode(u8 **out, u32 *lenOut, const u32 *in, u32 count)
     u32 *data = (u32 *)malloc(count * sizeof(u32));
 
     u32 usedBits = *(in++);
-    BitPackEncoder::scalar_decode(data, in, count, usedBits);
+    BitPackEncoder::scalar_decode(data, in, count, usedBits); // TODO better cache usage
+                                                              // when doing block compression i should
+                                                              // resuse data while its in l1 cache
     const u32 *initidx = in + words_used(count, usedBits);
 
     u32 idxcount = *(initidx++);
