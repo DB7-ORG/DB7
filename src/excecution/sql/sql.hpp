19aac067b429be966edf6de4903dfe6c779862e6
Jovan Katanic <jovan.katanic0101@gmail.com>
2026-04-16 10:18:35 +0200

    other parser stuff

--- src/excecution/sql/sql.hpp
+++ src/excecution/sql/sql.hpp
@@ -21,4 +21,37 @@ namespace noisepage::execution::sql
         Varchar,   // Variable-length string
         Varbinary  // TODO(Matt): front-end doesn't support this. See #788
     };
+
+    uint16_t GetSqlTypeIdSize(SqlTypeId type)
+    {
+        switch (type)
+        {
+        case SqlTypeId::Boolean:
+            return sizeof(bool);
+        case SqlTypeId::TinyInt:
+            return sizeof(int8_t);
+        case SqlTypeId::SmallInt:
+            return sizeof(int16_t);
+        case SqlTypeId::Integer:
+            return sizeof(int32_t);
+        case SqlTypeId::BigInt:
+            return sizeof(int64_t);
+            //    case SqlTypeId::Float:    // TODO(Matt): not supported on front-end
+            //      return sizeof(float);
+        case SqlTypeId::Double:
+            return sizeof(double);
+        case SqlTypeId::Date:
+            return sizeof(Date);
+        case SqlTypeId::Timestamp:
+            return sizeof(Timestamp);
+        case SqlTypeId::Varchar:
+        case SqlTypeId::Varbinary:
+            return storage::VARLEN_COLUMN;
+        case SqlTypeId::Decimal:
+            return 16; // TODO(Matt): double-check when fixed point decimal support merges
+        default:
+            // All cases handled
+            UNREACHABLE("Impossible type");
+        }
+    }
 }
